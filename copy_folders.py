504c83410f74c18208b19dc75bb97a06b7d94c4c
Bastian Awischus <bastianawischus@bastians-air.lan>
2020-09-25 23:13:57 -0700

    Created spreadsheetNamesList.txt, created spreadsheetNames list, created readFolderFromFile, Removed hardcoded portion and added for loop in each function to read over the list

--- copy_folders.py
+++ copy_folders.py
@@ -5,44 +5,39 @@
 # 3) Change the file names to their appropriate names
 # 4) Copy the file names back into needed Directory
 # -----------------------------------------------
+# 5) Create a function that can read the file names
+# 6) Itterate over name change of file see changeFileName function
+
 
 import os
-import xlrd
 from shutil import copy
 from shutil import rmtree
 
+spreadsheetNames = []
+
 path = "/Users/bastianawischus/Documents/Git/work_automation/"
 #path = os.getcwd
+# Use above line to work in current directory
 def createFolder():
     try:
-        #print(os.getcwd)
-        #print(path)
-        os.mkdir(path + "Folder1")
-        os.mkdir(path + "Folder2")
-        os.mkdir(path + "Folder3")
-        os.mkdir(path + "Folder4")
-        os.mkdir(path + "Folder5")
+        for x in spreadsheetNames:
+            os.mkdir(path + x)
     except:
         print("An error occured creating the Folder Stucture")
 
 def copyFiles():
     try:
-        copy(path + "/sourceFiles/TestSheet1.xlsx", path + "Folder1")
-        copy(path + "/sourceFiles/TestSheet2.xlsx", path + "Folder2")
-        copy(path + "/sourceFiles/TestSheet3.xlsx", path + "Folder3")
-        copy(path + "/sourceFiles/TestSheet4.xlsx", path + "Folder4")
-        copy(path + "/sourceFiles/TestSheet5.xlsx", path + "Folder5")
+        for x in spreadsheetNames:
+            copy(path + "/sourceFiles/TestSheet1.xlsx", path + x)
     except:
         print("An error occured coping the files")
 
-
 def changeFileName():
     try:
-        os.rename(path + "Folder1/TestSheet1.xlsx", path + "Folder1/NewName1.xlsx")
-        os.rename(path + "Folder2/TestSheet2.xlsx", path + "Folder2/NewName2.xlsx")
-        os.rename(path + "Folder3/TestSheet3.xlsx", path + "Folder3/NewName3.xlsx")
-        os.rename(path + "Folder4/TestSheet4.xlsx", path + "Folder4/NewName4.xlsx")
-        os.rename(path + "Folder5/TestSheet5.xlsx", path + "Folder5/NewName5.xlsx")
+        for x in spreadsheetNames:
+            os.rename(path + x + "/TestSheet1.xlsx", path + x + "/NewName1.xlsx")
+            # The NewName1 is hardcoded, this will cause all the files be called
+            # NewName1 and copied over. Need to figure out how to add indecies 
     except:
         print("An error occured changing file names")
 
@@ -51,24 +46,29 @@ def copyHome():
     try:
         if os.path.isdir(path + "HomeFolder") == False:
             os.mkdir(path + "HomeFolder")
-        copy(path + "Folder1/NewName1.xlsx", path + "HomeFolder")
-        copy(path + "Folder2/NewName2.xlsx", path + "HomeFolder")
-        copy(path + "Folder3/NewName3.xlsx", path + "HomeFolder")
-        copy(path + "Folder4/NewName4.xlsx", path + "HomeFolder")
-        copy(path + "Folder5/NewName5.xlsx", path + "HomeFolder")
+        for x in spreadsheetNames:
+            copy(path + x + "/NewName1.xlsx", path + "HomeFolder")
     except:
         print("An error occured copying the files to the Home directory")
 
 def cleanUpFolders():
     try:
-        rmtree(path + "Folder1")
-        rmtree(path + "Folder2")
-        rmtree(path + "Folder3")
-        rmtree(path + "Folder4")
-        rmtree(path + "Folder5")
+        for x in spreadsheetNames:
+            rmtree(path + x)
     except:
         print("An error occured cleaning up the directory structure")
 
+def readFolderNamesFromFile():
+    try:
+        f=open("spreadsheetNameList.txt", "r")
+        f1=f.readlines()
+        for x in f1:
+            spreadsheetNames.append(x)
+        #print(speadsheetNames) # ignore the \n. Reading the index it is not there
+    except:
+        print("Can not read spreadsheetNameList.txt")
+
+readFolderNamesFromFile()
 createFolder()
 copyFiles()
 changeFileName()
