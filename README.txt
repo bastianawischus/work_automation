1ed463e8d18aae1912ac48f8a6d342ffe3bbf10f
Bastian Awischus <bastianawischus@bastians-air.lan>
2020-09-25 22:16:00 -0700

    Created README and moved notes into README

---
+++ README.txt
@@ -0,0 +1,9 @@
+# NOTE: 
+# - The Directory structure needs to be updated to template structure. Its likely to remain static. 
+# - Not all these opperations need to happend in an instance. Implement passing prameter into these to execute the funtions.
+# - This is not fully automation as between S2 and S3 manual labor needs to get done. If this gets automated in the future then just call the sript in a updateTemplate function.
+# - This is a fast build prototype / proof of concept. I know how bad it is to have everything hard coded.
+# - I belive this does not work if the files or folders currently exist. Need to encapusate all logic in try catch.
+# Notes from Dan:
+# - Keep in mind when Adding speadsheet, get the names from a file, Speadsheet name -> Conversion Name.
+# - Speadsheet name changes
--- copy_folders.py
+++ copy_folders.py
@@ -5,17 +5,9 @@
 # 3) Change the file names to their appropriate names
 # 4) Copy the file names back into needed Directory
 # -----------------------------------------------
-# NOTE: 
-# - The Directory structure needs to be updated to template structure. Its likely to remain static. 
-# - Not all these opperations need to happend in an instance. Implement passing prameter into these to execute the funtions.
-# - This is not fully automation as between S2 and S3 manual labor needs to get done. If this gets automated in the future then just call the sript in a updateTemplate function.
-# - This is a fast build prototype / proof of concept. I know how bad it is to have everything hard coded.
-# - I belive this does not work if the files or folders currently exist. Need to encapusate all logic in try catch.
-
-# - Keep in mind when Adding speadsheet, get the names from a file, Speadsheet name -> Conversion Name.
-# - Speadsheet name changes
 
 import os
+import xlrd
 from shutil import copy
 from shutil import rmtree
 
