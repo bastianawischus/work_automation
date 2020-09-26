43b305c9226f5a0ba17036479b04899f403e04ad
Bastian Awischus <bastianawischus@bastians-air.lan>
2020-09-25 22:06:43 -0700

    Added Try and except blocks to all functions. Added logic to copyHome to check for home directory before creating.

--- copy_folders.py
+++ copy_folders.py
@@ -22,46 +22,63 @@ from shutil import rmtree
 path = "/Users/bastianawischus/Documents/Git/work_automation/"
 #path = os.getcwd
 def createFolder():
-    #print(path)
-    os.mkdir(path + "Folder1")
-    os.mkdir(path + "Folder2")
-    os.mkdir(path + "Folder3")
-    os.mkdir(path + "Folder4")
-    os.mkdir(path + "Folder5")
+    try:
+        #print(os.getcwd)
+        #print(path)
+        os.mkdir(path + "Folder1")
+        os.mkdir(path + "Folder2")
+        os.mkdir(path + "Folder3")
+        os.mkdir(path + "Folder4")
+        os.mkdir(path + "Folder5")
+    except:
+        print("An error occured creating the Folder Stucture")
 
 def copyFiles():
-    copy(path + "/sourceFiles/TestSheet1.xlsx", path + "Folder1")
-    copy(path + "/sourceFiles/TestSheet2.xlsx", path + "Folder2")
-    copy(path + "/sourceFiles/TestSheet3.xlsx", path + "Folder3")
-    copy(path + "/sourceFiles/TestSheet4.xlsx", path + "Folder4")
-    copy(path + "/sourceFiles/TestSheet5.xlsx", path + "Folder5")
+    try:
+        copy(path + "/sourceFiles/TestSheet1.xlsx", path + "Folder1")
+        copy(path + "/sourceFiles/TestSheet2.xlsx", path + "Folder2")
+        copy(path + "/sourceFiles/TestSheet3.xlsx", path + "Folder3")
+        copy(path + "/sourceFiles/TestSheet4.xlsx", path + "Folder4")
+        copy(path + "/sourceFiles/TestSheet5.xlsx", path + "Folder5")
+    except:
+        print("An error occured coping the files")
 
 
 def changeFileName():
-    os.rename(path + "Folder1/TestSheet1.xlsx", path + "Folder1/NewName1.xlsx")
-    os.rename(path + "Folder2/TestSheet2.xlsx", path + "Folder2/NewName2.xlsx")
-    os.rename(path + "Folder3/TestSheet3.xlsx", path + "Folder3/NewName3.xlsx")
-    os.rename(path + "Folder4/TestSheet4.xlsx", path + "Folder4/NewName4.xlsx")
-    os.rename(path + "Folder5/TestSheet5.xlsx", path + "Folder5/NewName5.xlsx")
+    try:
+        os.rename(path + "Folder1/TestSheet1.xlsx", path + "Folder1/NewName1.xlsx")
+        os.rename(path + "Folder2/TestSheet2.xlsx", path + "Folder2/NewName2.xlsx")
+        os.rename(path + "Folder3/TestSheet3.xlsx", path + "Folder3/NewName3.xlsx")
+        os.rename(path + "Folder4/TestSheet4.xlsx", path + "Folder4/NewName4.xlsx")
+        os.rename(path + "Folder5/TestSheet5.xlsx", path + "Folder5/NewName5.xlsx")
+    except:
+        print("An error occured changing file names")
 
 def copyHome():
     # For testing purpose only, instead of creating a HomeFolder we can copy them into a Source File.
-    os.mkdir(path + "HomeFolder")
-    copy(path + "Folder1/NewName1.xlsx", path + "HomeFolder")
-    copy(path + "Folder2/NewName2.xlsx", path + "HomeFolder")
-    copy(path + "Folder3/NewName3.xlsx", path + "HomeFolder")
-    copy(path + "Folder4/NewName4.xlsx", path + "HomeFolder")
-    copy(path + "Folder5/NewName5.xlsx", path + "HomeFolder")
+    try:
+        if os.path.isdir(path + "HomeFolder") == False:
+            os.mkdir(path + "HomeFolder")
+        copy(path + "Folder1/NewName1.xlsx", path + "HomeFolder")
+        copy(path + "Folder2/NewName2.xlsx", path + "HomeFolder")
+        copy(path + "Folder3/NewName3.xlsx", path + "HomeFolder")
+        copy(path + "Folder4/NewName4.xlsx", path + "HomeFolder")
+        copy(path + "Folder5/NewName5.xlsx", path + "HomeFolder")
+    except:
+        print("An error occured copying the files to the Home directory")
 
 def cleanUpFolders():
-    rmtree(path + "Folder1")
-    rmtree(path + "Folder2")
-    rmtree(path + "Folder3")
-    rmtree(path + "Folder4")
-    rmtree(path + "Folder5")
+    try:
+        rmtree(path + "Folder1")
+        rmtree(path + "Folder2")
+        rmtree(path + "Folder3")
+        rmtree(path + "Folder4")
+        rmtree(path + "Folder5")
+    except:
+        print("An error occured cleaning up the directory structure")
 
-#createFolder()
-#copyFiles()
-#changeFileName()
-#copyHome()
-#cleanUpFolders()
+createFolder()
+copyFiles()
+changeFileName()
+copyHome()
+cleanUpFolders()
